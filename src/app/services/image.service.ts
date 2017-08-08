0e34d6987034e9d7841b0dea494009b9da3b3ded
aurimas-darguzis <aurimas@darguzis.com>
2017-08-08 21:57:26 +0100

    importing angularfire2 with modules

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@angular/core';
+import { GalleryImage } from './../models/galleryImage.model';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { FirebaseApp } from 'angularfire2';
+import 'firebase/storage';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class ImageService {
