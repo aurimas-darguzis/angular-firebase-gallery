004a2ee4fd009403bccfb06e6523167d32beaaea
aurimas-darguzis <aurimas@darguzis.com>
2017-08-11 07:40:58 +0100

    implemented authentication service

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,10 +1,23 @@
+import { Observable } from 'rxjs/Observable';
+import { User } from './../models/user.model';
 import { AngularFireAuth } from 'angularfire2/auth';
-
+import * as firebase from 'firebase/app';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthenticationService {
 
-  constructor(private af: AngularFireAuth) { }
+  private user: Observable<firebase.User>;
 
+  constructor(private afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+   }
+
+  login(user: User) {
+    return this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
+
+  logout() {
+    return this.afAuth.auth.signOut();
+  }
 }
