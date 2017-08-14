b57bffe036aa3f417e159f8091e516c27e68fed5
aurimas-darguzis <aurimas@darguzis.com>
2017-08-14 22:06:58 +0100

    fixed bug

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angula
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   userForm: FormGroup;
   email: string;
   password: string;
@@ -16,70 +16,70 @@ export class LoginComponent implements OnInit {
 
   constructor(private authS: AuthenticationService, private router: Router, private fb: FormBuilder) { }
 
-  ngOnInit () {
-    this.buildForm();
-  }
-
-  buildForm(): void {
-     this.userForm = this.fb.group({
-       'email': ['', [
-          //  Validators.required,
-          //  Validators.email
-         ]
-       ],
-       'password': ['', [
-         Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
-         Validators.minLength(6),
-         Validators.maxLength(25)
-       ]
-     ],
-     });
-
-     this.userForm.valueChanges.subscribe(data => this.onValueChanged(data));
-     this.onValueChanged(); // reset validation messages
-   }
-
-    // Updates validation state on form changes.
-   onValueChanged(data?: any) {
-     if (!this.userForm) { return; }
-     const form = this.userForm;
-     for (const field in this.formErrors) {
-       if (field) {
-
-       }
-       // clear previous error message (if any)
-       this.formErrors[field] = '';
-       const control = form.get(field);
-       if (control && control.dirty && !control.valid) {
-         const messages = this.validationMessages[field];
-         for (const key in control.errors) {
-           this.formErrors[field] += messages[key] + ' ';
-         }
-       }
-     }
-   }
-
-  formErrors = {
-     'email': '',
-     'password': ''
-   };
-
-   validationMessages = {
-     'email': {
-       'required':      'Email is required.',
-       'email':         'Email must be a valid email'
-     },
-     'password': {
-       'required':      'Password is required.',
-       'pattern':       'Password must be include at one letter and one number.',
-       'minlength':     'Password must be at least 4 characters long.',
-       'maxlength':     'Password cannot be more than 40 characters long.',
-     }
-   };
-
- signIn() {
+signIn() {
    this.authS.login({ email: this.email, password: this.password })
     .then(resolve => this.router.navigate(['gallery']))
     .catch(error => this.errorMsg = error.message);
  }
+
+  // ngOnInit () {
+  //   this.buildForm();
+  // }
+
+  // buildForm(): void {
+  //    this.userForm = this.fb.group({
+  //      'email': ['', [
+  //           //  Validators.required,
+  //           //  Validators.email
+  //        ]
+  //      ],
+  //      'password': ['', [
+  //        Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
+  //        Validators.minLength(6),
+  //        Validators.maxLength(25)
+  //      ]
+  //    ],
+  //    });
+
+  //    this.userForm.valueChanges.subscribe(data => this.onValueChanged(data));
+  //    this.onValueChanged(); // reset validation messages
+  //  }
+
+  //   // Updates validation state on form changes.
+  //  onValueChanged(data?: any) {
+  //    if (!this.userForm) { return; }
+  //    const form = this.userForm;
+  //    for (const field in this.formErrors) {
+  //      if (field) {
+
+  //      }
+  //      // clear previous error message (if any)
+  //      this.formErrors[field] = '';
+  //      const control = form.get(field);
+  //      if (control && control.dirty && !control.valid) {
+  //        const messages = this.validationMessages[field];
+  //        for (const key in control.errors) {
+  //          this.formErrors[field] += messages[key] + ' ';
+  //        }
+  //      }
+  //    }
+  //  }
+
+  // formErrors = {
+  //    'email': '',
+  //    'password': ''
+  //  };
+
+  //  validationMessages = {
+  //    'email': {
+  //      'required':      'Email is required.',
+  //      'email':         'Email must be a valid email'
+  //    },
+  //    'password': {
+  //      'required':      'Password is required.',
+  //      'pattern':       'Password must be include at one letter and one number.',
+  //      'minlength':     'Password must be at least 4 characters long.',
+  //      'maxlength':     'Password cannot be more than 40 characters long.',
+  //    }
+  //  };
 }
