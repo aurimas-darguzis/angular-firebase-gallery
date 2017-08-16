c514b3efb9533506849a84109e121b8078e474d8
aurimas-darguzis <aurimas@darguzis.com>
2017-08-16 07:50:13 +0100

    working on upload service method

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -1,8 +1,20 @@
+import { Upload } from './../models/upload.model';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { GalleryImage } from './../models/galleryImage.model';
+import { AngularFireModule } from 'angularfire2';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UploadService {
 
-  constructor() { }
+  private basePath = '/uploads';
+  private uploads: FirebaseListObservable<GalleryImage[]>;
+
+  constructor(private ngFire: AngularFireModule, private db: AngularFireDatabase) { }
+
+  uploadFile (upload: Upload) {
+    const storageRef = firebase.storage().ref();
+    const putUpload = storageRef.child(`${this.basePath}/${upload.file.name}`).put(upload.file);
+  }
 
 }
