d627501affc591cd8916752e8748ae70a7a2508a
aurimas-darguzis <aurimas@darguzis.com>
2017-08-19 14:49:52 +0100

    getImage function added to img servc

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -25,4 +25,9 @@ export class ImageService {
     return this.db.list('uploads');
   }
 
+  getImage(key: string) {
+    return firebase.database().ref('upload/' + key).once('value')
+      .then((snap) => snap.val());
+  }
+
 }
